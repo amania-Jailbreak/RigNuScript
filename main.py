93c2fa7c7f76641c828210980b2d10b0eff015e7
amania-Jailbreak <amania.apple@icloud.com>
2024-02-15 21:03:57 +0900

    テストスクリプトの作成+いろいろ

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Loading....")
 try:
-    import os,sys,subprocess,configparser
+    import os,sys,subprocess,configparser,sys
 except:
     print("モジュールの読み込みに失敗しました")
 try:
@@ -9,23 +9,36 @@ try:
 except:
     print("設定ファイルを読み込めませんでした")
 
+def write_env_path_to_temp_ini(path):
+    try:
+        with open("temp.ini", "w") as temp_ini_file:
+            temp_ini_file.write("[env]\npath = " + path)
+    except Exception as e:
+        print("temp.iniファイルにパスを書き込めませんでした")
+
+write_env_path_to_temp_ini(sys.argv[1])
+
 print("""
 ---------------------------------------------------------------------------
 RigNuScript Ver 1.0
-
-a = script_test
 """)
+print("loading script")
+files = [file for file in os.listdir("./script") if file.endswith(".py")]
+names = []
+for file in files:
+    names.append(file.split(".")[0])
+for index, name in enumerate(names):
+    print(f"{index} : {name}")
 c = input(">>>")
-if c == "a":
-    print("loading script")
-    files = [file for file in os.listdir("./script") if file.endswith(".py")]
-    names = []
-    for file in files:
-        with open(os.path.join("./script", file), 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                if 'name:' in line:
-                    name = line.split('name:')[1].strip()
-                    names.append(name)
-    for i in names:
-        print(i)
+if c.isdigit() and int(c) < len(names):
+    selected_name = names[int(c)]
+    selected_path = os.path.join("./script", f"{selected_name}.py")
+    try:
+        with open(selected_path, 'r', encoding="utf-8") as selected_file:
+            selected_code = selected_file.read()
+            exec(selected_code)
+    except FileNotFoundError:
+        print("選択されたスクリプトが見つかりません")
+else:
+    print("無効な番号が入力されました")
+
--- script/test.py
+++ script/test.py
@@ -1,6 +1,55 @@
-import os
-import rns
+"""
+name:Resize
+Author:amania
+"""
+print("-----------------------------------------------------------------")
 print("Resize Script")
 print("このモジュールでは現在の解像度を100%とし拡大と縮小を行うことができます")
 print("警告:Configではデフォルトでバックアップが無効になっております バックアップを有効にするにはRNSのルートにあるconfigフォルダの中にあるResize.iniを編集してください")
-RNS = rns.RNS()
+print("-----------------------------------------------------------------")
+try:
+    import os
+    import rns
+    import cv2
+except:
+    print("モジュールの読み込みに失敗しました")
+try:
+    root = rns.RNS()
+    path = root.env["path"]
+except:
+    print("RNSモジュールの初期化に失敗しました 実行環境とRNSの設定などを再確認しそれでもダメな場合は再インストールしてください")
+try:
+    config = rns.Config("Resize")
+    if "setting" in config.config:
+        if "backup" in config.config["setting"]:
+            backup = config.config["setting"]["backup"]
+        else:
+            config.config["setting"]["backup"] = "False"
+            backup = "False"
+            config.save()
+    else:
+        config.config["setting"] = {}
+        config.config["setting"]["backup"] = "False"
+        config.save()
+        backup = "False"
+except Exception as e:
+    print(e)
+    print("設定を読み込めませんでした")
+def resize_image(path, scale_factor, backup="False"):
+    image = cv2.imread(path)
+    if backup == "True":
+        backup_path_t = path.split(".")
+        backup_path = backup_path_t[0] + ".back." + backup_path_t[1]
+        cv2.imwrite(backup_path, image)
+    
+    height, width = image.shape[:2]
+    new_height = int(height * scale_factor)
+    new_width = int(width * scale_factor)
+
+    resized_image = cv2.resize(image, (new_width, new_height))
+    print(path)
+    cv2.imwrite(path, resized_image)
+scale_factor = float(input("パーセントを入力してください: ")) / 100
+resize_image(path, scale_factor, backup)
+
+
